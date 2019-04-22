0d71c02fe524d65be63c154c2db81a158dcfbd2d
jingxigao <jingxi.gao@tum,de>
2019-04-22 21:42:00 +0800

    pages cli 更新

---
+++ src/components/Login.vue
@@ -0,0 +1,34 @@
+<template>
+  <el-container>
+    <div>统一开发平台</div>
+    <div>Unified Development Platform</div>
+    <div>
+      <div>欢迎登陆</div>
+      <el-input>请输入登录账号</el-input>
+      <el-input>请输入登录密码</el-input>
+    </div>
+  </el-container>
+</template>
+
+<script>
+export default {
+  name: 'Login',
+  data () {
+    return {
+    }
+  }
+}
+</script>
+
+<style scoped>
+  .el-container {
+    display: block;
+    position: absolute;
+    top: 0;
+    bottom: 0;
+    left: 0;
+    right: 0;
+    background-image: url(../images/bg-2.png);
+    /*background-size: contain;*/
+  }
+</style>
